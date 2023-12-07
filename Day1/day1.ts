51e589093227ee7bf8310210fc764f1779a9437b
joran <joran@lifely.nl>
2023-12-07 17:02:34 +0100

    feat(day1): add day1 solution and setup

---
+++ Day1/day1.ts
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+import { isNumberObject } from 'util/types';
+
+const input = fs.readFileSync('./input.txt', 'utf8');
+
+const lines = input.split('\n');
+let numberOut = 0;
+for (const line of lines) {
+  const numbers = line.split('').filter((c) => parseInt(c));
+  if (!numbers || !numbers.length) continue;
+  numberOut =
+    numberOut + parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`);
+}
+
+console.log(numberOut);
